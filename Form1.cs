d6e7c527f35e710eb2cf2fd1f110721a3c3421fb
XDRosenheim <patrickrosenheim@gmail.com>
2015-11-11 14:59:38 +0100

    It works :smile:

--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ namespace Fallout4MoreConfig
     {
         public Form1()
         {
+            Application.CurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("en-US");
             InitializeComponent();
             // TODO Get values from config files
             HUDOpacityValue.Text = "GET VALUE FROM FILE";
@@ -20,18 +21,24 @@ namespace Fallout4MoreConfig
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            ///
-            /// TODO Save to file(s)
-            /// 
-            
+            /// ATTENTION !!
+            /// THE FOLLOWING IS A MESS !!
+            /// IT READ THE WHOLE FILE TO MEMORY !!
+            /// PLEASE CONTACT ME ON GITHUB IF YOU HAVE A SOLUTION !!
+            /// But it works...
+
+            // File path
+            string fallout4PrefsLocation = Environment.ExpandEnvironmentVariables("%HOMEPATH%") 
+                + "\\Documents\\My Games\\fallout4\\Fallout4Prefs.ini";
+            string text = File.ReadAllText(fallout4PrefsLocation);
             // HUD - opacity
-            string path = Environment.ExpandEnvironmentVariables("%HOMEPATH%") + "\\Documents\\My Games\\fallout4\\Fallout4Prefs.ini";
-            string text = File.ReadAllText(path);
-            string pattern = @"fHUDOpacity=([A-Za-z0-9\-]+)";
-            string replacement = "fHUDOpacity=" + (trackHudOpacity.Value / 100);
+            string pattern = @"fHUDOpacity=([A-Za-z0-9\-\.]+)";
+            string replacement = "fHUDOpacity=" + Math.Round(Convert.ToDouble(trackHudOpacity.Value) / 100, 4);
             Regex rgx = new Regex(pattern);
             string result = rgx.Replace(text, replacement);
-            File.WriteAllText(path, result);
+            
+            // Write to file
+            File.WriteAllText(fallout4PrefsLocation, result);
         }
 
         private void btnOMFGQUIT_Click(object sender, EventArgs e)
