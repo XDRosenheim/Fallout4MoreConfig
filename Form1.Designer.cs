a2d0dffa6a6c3119cfd5c17f2f886a5e1703a100
XDRosenheim <patrickrosenheim@gmail.com>
2015-11-12 09:01:52 +0100

    This should help with line endings.
    Some says.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,16 +32,20 @@
             this.tabVisuals = new System.Windows.Forms.TabPage();
             this.tabAudio = new System.Windows.Forms.TabPage();
             this.tabSettings = new System.Windows.Forms.TabPage();
+            this.textBoxAutoSave = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabHUD = new System.Windows.Forms.TabPage();
-            this.trackHudOpacity = new System.Windows.Forms.TrackBar();
-            this.HUDOpacityLabel = new System.Windows.Forms.Label();
             this.HUDOpacityValue = new System.Windows.Forms.Label();
+            this.HUDOpacityLabel = new System.Windows.Forms.Label();
+            this.trackHudOpacity = new System.Windows.Forms.TrackBar();
+            this.tabPipBoy = new System.Windows.Forms.TabPage();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnDefault = new System.Windows.Forms.Button();
             this.btnOMFGQUIT = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.tabPipBoy = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
+            this.tabSettings.SuspendLayout();
             this.tabHUD.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackHudOpacity)).BeginInit();
             this.SuspendLayout();
@@ -68,6 +72,7 @@
             this.tabVisuals.Cursor = System.Windows.Forms.Cursors.Default;
             this.tabVisuals.Location = new System.Drawing.Point(4, 22);
             this.tabVisuals.Name = "tabVisuals";
+            this.tabVisuals.Padding = new System.Windows.Forms.Padding(3);
             this.tabVisuals.Size = new System.Drawing.Size(318, 207);
             this.tabVisuals.TabIndex = 0;
             this.tabVisuals.Text = "Visuals";
@@ -79,6 +84,7 @@
             this.tabAudio.Cursor = System.Windows.Forms.Cursors.Default;
             this.tabAudio.Location = new System.Drawing.Point(4, 22);
             this.tabAudio.Name = "tabAudio";
+            this.tabAudio.Padding = new System.Windows.Forms.Padding(3);
             this.tabAudio.Size = new System.Drawing.Size(318, 207);
             this.tabAudio.TabIndex = 1;
             this.tabAudio.Text = "Audio";
@@ -87,11 +93,31 @@
             // 
             this.tabSettings.BackColor = System.Drawing.Color.Transparent;
             this.tabSettings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.tabSettings.Controls.Add(this.label2);
+            this.tabSettings.Controls.Add(this.textBoxAutoSave);
+            this.tabSettings.Controls.Add(this.label1);
             this.tabSettings.Location = new System.Drawing.Point(4, 22);
             this.tabSettings.Name = "tabSettings";
+            this.tabSettings.Padding = new System.Windows.Forms.Padding(3);
             this.tabSettings.Size = new System.Drawing.Size(318, 207);
             this.tabSettings.TabIndex = 2;
             this.tabSettings.Text = "Settings";
+            // 
+            // textBoxAutoSave
+            // 
+            this.textBoxAutoSave.Location = new System.Drawing.Point(8, 19);
+            this.textBoxAutoSave.Name = "textBoxAutoSave";
+            this.textBoxAutoSave.Size = new System.Drawing.Size(56, 20);
+            this.textBoxAutoSave.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auto-save";
             // 
             // tabHUD
             // 
@@ -103,38 +129,53 @@
             this.tabHUD.Cursor = System.Windows.Forms.Cursors.Default;
             this.tabHUD.Location = new System.Drawing.Point(4, 22);
             this.tabHUD.Name = "tabHUD";
+            this.tabHUD.Padding = new System.Windows.Forms.Padding(3);
             this.tabHUD.Size = new System.Drawing.Size(318, 207);
             this.tabHUD.TabIndex = 3;
             this.tabHUD.Text = "HUD";
             // 
-            // trackHudOpacity
+            // HUDOpacityValue
             // 
-            this.trackHudOpacity.Location = new System.Drawing.Point(8, 33);
-            this.trackHudOpacity.Maximum = 100;
-            this.trackHudOpacity.Name = "trackHudOpacity";
-            this.trackHudOpacity.Size = new System.Drawing.Size(95, 45);
-            this.trackHudOpacity.TabIndex = 1;
-            this.trackHudOpacity.TickStyle = System.Windows.Forms.TickStyle.None;
-            this.trackHudOpacity.Value = 100;
-            this.trackHudOpacity.Scroll += new System.EventHandler(this.trackHudOpacity_Scroll);
+            this.HUDOpacityValue.AutoSize = true;
+            this.HUDOpacityValue.Location = new System.Drawing.Point(104, 25);
+            this.HUDOpacityValue.Name = "HUDOpacityValue";
+            this.HUDOpacityValue.Size = new System.Drawing.Size(33, 13);
+            this.HUDOpacityValue.TabIndex = 3;
+            this.HUDOpacityValue.Text = "100%";
             // 
             // HUDOpacityLabel
             // 
             this.HUDOpacityLabel.AutoSize = true;
-            this.HUDOpacityLabel.Location = new System.Drawing.Point(9, 14);
+            this.HUDOpacityLabel.Location = new System.Drawing.Point(11, 3);
             this.HUDOpacityLabel.Name = "HUDOpacityLabel";
             this.HUDOpacityLabel.Size = new System.Drawing.Size(43, 13);
             this.HUDOpacityLabel.TabIndex = 2;
             this.HUDOpacityLabel.Text = "Opacity";
             // 
-            // HUDOpacityValue
+            // trackHudOpacity
             // 
-            this.HUDOpacityValue.AutoSize = true;
-            this.HUDOpacityValue.Location = new System.Drawing.Point(110, 33);
-            this.HUDOpacityValue.Name = "HUDOpacityValue";
-            this.HUDOpacityValue.Size = new System.Drawing.Size(33, 13);
-            this.HUDOpacityValue.TabIndex = 3;
-            this.HUDOpacityValue.Text = "100%";
+            this.trackHudOpacity.AutoSize = false;
+            this.trackHudOpacity.LargeChange = 10;
+            this.trackHudOpacity.Location = new System.Drawing.Point(3, 19);
+            this.trackHudOpacity.Maximum = 100;
+            this.trackHudOpacity.Name = "trackHudOpacity";
+            this.trackHudOpacity.Size = new System.Drawing.Size(100, 24);
+            this.trackHudOpacity.TabIndex = 1;
+            this.trackHudOpacity.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackHudOpacity.Value = 100;
+            this.trackHudOpacity.Scroll += new System.EventHandler(this.trackHudOpacity_Scroll);
+            // 
+            // tabPipBoy
+            // 
+            this.tabPipBoy.BackColor = System.Drawing.Color.Transparent;
+            this.tabPipBoy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.tabPipBoy.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tabPipBoy.Location = new System.Drawing.Point(4, 22);
+            this.tabPipBoy.Name = "tabPipBoy";
+            this.tabPipBoy.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPipBoy.Size = new System.Drawing.Size(318, 207);
+            this.tabPipBoy.TabIndex = 4;
+            this.tabPipBoy.Text = "Pip-Boy";
             // 
             // btnSave
             // 
@@ -174,16 +215,14 @@
             this.button4.Text = "Button9002";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // tabPipBoy
+            // label2
             // 
-            this.tabPipBoy.BackColor = System.Drawing.Color.Transparent;
-            this.tabPipBoy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.tabPipBoy.Cursor = System.Windows.Forms.Cursors.Default;
-            this.tabPipBoy.Location = new System.Drawing.Point(4, 22);
-            this.tabPipBoy.Name = "tabPipBoy";
-            this.tabPipBoy.Size = new System.Drawing.Size(318, 207);
-            this.tabPipBoy.TabIndex = 4;
-            this.tabPipBoy.Text = "Pip-Boy";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Minutes";
             // 
             // Form1
             // 
@@ -199,6 +238,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
+            this.tabSettings.ResumeLayout(false);
+            this.tabSettings.PerformLayout();
             this.tabHUD.ResumeLayout(false);
             this.tabHUD.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackHudOpacity)).EndInit();
@@ -221,6 +262,9 @@
         private System.Windows.Forms.Button btnOMFGQUIT;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TabPage tabPipBoy;
+        private System.Windows.Forms.TextBox textBoxAutoSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,18 +7,20 @@ namespace Fallout4MoreConfig
 {
     public partial class Form1 : Form
     {
+        public string fallout4PrefsLocation = Environment.ExpandEnvironmentVariables("%HOMEPATH%")
+                + "\\Documents\\My Games\\fallout4\\Fallout4Prefs.ini";
+
         public Form1()
         {
             Application.CurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("en-US");
             InitializeComponent();
             // TODO Get values from config files
             HUDOpacityValue.Text = "GET VALUE FROM FILE";
-        }
 
+        }
         private void trackHudOpacity_Scroll(object sender, EventArgs e) { 
             HUDOpacityValue.Text = trackHudOpacity.Value.ToString() + "%"; 
         }
-
         private void btnSave_Click(object sender, EventArgs e)
         {
             /// ATTENTION !!
@@ -28,22 +30,17 @@ namespace Fallout4MoreConfig
             /// But it works...
 
             // File path
-            string fallout4PrefsLocation = Environment.ExpandEnvironmentVariables("%HOMEPATH%") 
-                + "\\Documents\\My Games\\fallout4\\Fallout4Prefs.ini";
+            
             string text = File.ReadAllText(fallout4PrefsLocation);
             // HUD - opacity
-            string pattern = @"fHUDOpacity=([A-Za-z0-9\-\.]+)";
+            string pattern = @"fHUDOpacity=([0-9\.]+)";
             string replacement = "fHUDOpacity=" + Math.Round(Convert.ToDouble(trackHudOpacity.Value) / 100, 4);
             Regex rgx = new Regex(pattern);
-            string result = rgx.Replace(text, replacement);
+            text = rgx.Replace(text, replacement);
             
             // Write to file
-            File.WriteAllText(fallout4PrefsLocation, result);
+            File.WriteAllText(fallout4PrefsLocation, text);
         }
-
-        private void btnOMFGQUIT_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
+        private void btnOMFGQUIT_Click(object sender, EventArgs e) { Application.Exit(); }
     }
 }
